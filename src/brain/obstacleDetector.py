aa4d8281ccc7fc10424d998497968202be3e4f2e
seowyanyi <seow.yanyi@gmail.com>
2015-09-15 23:41:38 +0800

    more queues

--- src/brain/obstacleDetector.py
+++ src/brain/obstacleDetector.py
@@ -1,14 +1,18 @@
+from src.arduinoInterface import actuators
 import threading
 import time
 
 class ObstacleDetectorThread(threading.Thread):
-    def __init__(self, threadName, delaySeconds, middleSonarQueue, leftSonarQueue, rightSonarQueue):
+    def __init__(self, threadName, delaySeconds, middleSonarQueue,
+                 leftSonarQueue, rightSonarQueue, hapticQueue, audioQueue):
         threading.Thread.__init__(self)
         self.threadName = threadName
         self.delaySeconds = delaySeconds
         self.middleSonarQueue = middleSonarQueue
         self.leftSonarQueue = leftSonarQueue
         self.rightSonarQueue = rightSonarQueue
+        self.hapticQueue = hapticQueue
+        self.audioQueue = audioQueue
 
     def run(self):
         print 'Starting {} thread'.format(self.threadName)
@@ -21,6 +25,13 @@ class ObstacleDetectorThread(threading.Thread):
             print '{}: received {} on right sonar'.format(self.threadName, rightSonarData)
             middleSonarData = self.middleSonarQueue.get()
             print '{}: received {} on middle sonar'.format(self.threadName, middleSonarData)
+
+            if counter % 2 == 0:
+                self.hapticQueue.put("buzzz")
+
+            if counter % 3 == 0:
+                self.hapticQueue.put("buzzzzzzzzzzzzzzzzz")
+
             time.sleep(self.delaySeconds)
             if counter > 15:
                 break
