c8642cf8fe21b971d21bdaf5f09bf36d02efcc3d
seowyanyi <seow.yanyi@gmail.com>
2015-10-10 11:56:23 +0800

    detect when user makes a turn

--- src/brain/pedometer.py
+++ src/brain/pedometer.py
@@ -9,12 +9,17 @@ import numpy as np
 import Queue
 import os
 import src.communication.queueManager as qm
+import time
 
 test_queue = Queue.Queue()
 
 WINDOW_SIZE = 5
 AT_REST_LIMIT = 1
 SWING_LIMIT = 2
+WINDOW_SIZE_BEARING = 5
+SECS_BETW_BEARING_READINGS = 1
+TURNING_THRESHOLD = 10
+
 #todo: minus ~5 degrees from bearing due to angle of foot
 
 import threading
@@ -43,36 +48,57 @@ def init_test_queue():
 def start_pedometer_processing(dataQueue, pedometerQueue, windowSize, atRestLimit, swingLimit, debug):
     steps = 0
     data = []
+    previous_bearing = None
+    time_bearing_taken = None
+    recent_bearings = []
 
     swing_count = 0
-    at_rest_count = 0
+    momentarily_at_rest_count = 0
     previouslyAtRest = True
 
     while True:
         if debug and dataQueue.qsize() <= 1:
             break
 
+        # Get data from imu and populate the relevant lists
         imuData = dataQueue.get(True)
         x = imuData.xAxis
         heading = imuData.heading
 
+        if len(recent_bearings) > WINDOW_SIZE_BEARING:
+            recent_bearings.pop(0)
+        recent_bearings.append(heading)
+
+        if previous_bearing is None:
+            previous_bearing = np.mean(recent_bearings)
+            time_bearing_taken = time.time()
+
         if len(data) > windowSize:
             data.pop(0)
-
         data.append(x)
-
         if len(data) < windowSize:
             continue
 
+        # check whether we are making a turn
+        if time.time() - time_bearing_taken >= SECS_BETW_BEARING_READINGS:
+            time_bearing_taken = time.time()
+            current_bearing = np.mean(recent_bearings)
+            if abs(current_bearing - previous_bearing) > TURNING_THRESHOLD:
+                previous_bearing = current_bearing
+                pedometerQueue.put({'type': Step.TURN, 'actual_bearing': current_bearing})
+                print 'USER IS MAKING A TURN'
+                continue
+
+        # detect the movement type
         if is_downward_swing(data):
             swing_count += 1
         else:
             swing_count = 0
 
-        if is_at_rest(data):
-            at_rest_count += 1
+        if is_momentarily_at_rest(data):
+            momentarily_at_rest_count += 1
         else:
-            at_rest_count = 0
+            momentarily_at_rest_count = 0
 
         if debug:
             line = get_equation_of_line(data)
@@ -80,19 +106,19 @@ def start_pedometer_processing(dataQueue, pedometerQueue, windowSize, atRestLimi
             write_to_gradient_file(gradient)
 
         # count as at rest between steps
-        if at_rest_count > atRestLimit:
+        if momentarily_at_rest_count > atRestLimit:
             previouslyAtRest = True
-            at_rest_count = 0
+            momentarily_at_rest_count = 0
 
         if previouslyAtRest and swing_count > swingLimit:
             steps += 1
-            pedometerQueue.put({'type': Step.FORWARD, 'actual_bearing': heading})
+            pedometerQueue.put({'type': Step.FORWARD, 'actual_bearing': np.mean(recent_bearings)})
             print 'NUMBER OF STEPS: {}'.format(steps)
-            print 'CURRENT HEADING: {} degrees\n'.format(heading)
+            print 'CURRENT HEADING: {} degrees\n'.format(np.mean(recent_bearings))
 
             swing_count = 0
             previouslyAtRest = False
-            at_rest_count = 0
+            momentarily_at_rest_count = 0
             if debug:
                 write_to_step_file('1')
         elif debug:
@@ -117,7 +143,7 @@ def is_downward_swing(data):
     gradient = line[0]
     return gradient < -0.30
 
-def is_at_rest(data):
+def is_momentarily_at_rest(data):
     line = get_equation_of_line(data)
     gradient = abs(line[0])
     return gradient <= 0.15
