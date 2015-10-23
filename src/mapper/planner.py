938b01672473335f3f066e596d20c6c31fc5bd66
Malavika Menon <malavika94@yahoo.com.sg>
2015-10-23 16:17:00 +0800

    Test planner.py

--- src/mapper/planner.py
+++ src/mapper/planner.py
@@ -83,20 +83,20 @@ def internet_on():
     return False
 
 def download_map(buildingName, levelNum):
-    try:
-        response=urllib2.urlopen('http://showmyway.comp.nus.edu.sg/getMapInfo.php?Building={}&Level={}',timeout=1).format(buildingName,levelNum)
-        mapJsonData = json.load(urllib2.urlopen(url))
-    except urllib2.URLError as err:
-        if (buildingName == 1 and levelNum == 2):
-        #            /Users/malavikamenon/IndoorNavigation/src/mapper/PreLoadedMaps/COM1Lvl2.json
-            with open('/home/pi/IndoorNavigation/src/mapper/PreLoadedMaps/COM1Lvl2.json') as json_file:
-                mapJsonData = json.load(json_file)
-        elif (buildingName == 2 and levelNum == 2):
-            with open('/home/pi/IndoorNavigation/src/mapper/PreLoadedMaps/COM2Lvl2.json') as json_file:
-                mapJsonData = json.load(json_file)
-        elif (buildingName == 2 and levelNum == 3):
-            with open('/home/pi/IndoorNavigation/src/mapper/PreLoadedMaps/COM2Lvl3.json') as json_file:
-                mapJsonData = json.load(json_file)
+#    try:
+#        response=urllib2.urlopen('http://showmyway.comp.nus.edu.sg/getMapInfo.php?Building={}&Level={}',timeout=1).format(buildingName,levelNum)
+#        mapJsonData = json.load(urllib2.urlopen(url))
+#    except urllib2.URLError as err:
+    if (buildingName == 1 and levelNum == 2):
+    #            /Users/malavikamenon/IndoorNavigation/src/mapper/PreLoadedMaps/COM1Lvl2.json
+        with open('/home/pi/IndoorNavigation/src/mapper/PreLoadedMaps/COM1Lvl2.json') as json_file:
+            mapJsonData = json.load(json_file)
+    elif (buildingName == 2 and levelNum == 2):
+        with open('/home/pi/IndoorNavigation/src/mapper/PreLoadedMaps/COM2Lvl2.json') as json_file:
+            mapJsonData = json.load(json_file)
+    elif (buildingName == 2 and levelNum == 3):
+        with open('/home/pi/IndoorNavigation/src/mapper/PreLoadedMaps/COM2Lvl3.json') as json_file:
+            mapJsonData = json.load(json_file)
 
     if mapJsonData["info"] is None:
         raise ValueError(MapError)
