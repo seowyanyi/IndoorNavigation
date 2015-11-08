fc0c91c5f5285e9fb2b538ec991d8cbc4b5f2a0a
seowyanyi <seow.yanyi@gmail.com>
2015-11-08 21:12:58 +0800

    recalibrate

--- src/brain/routeManager.py
+++ src/brain/routeManager.py
@@ -33,7 +33,7 @@ CURRENT_CHECKPOINT = 'Current checkpoint {}. {}'
 PRESS_TO_START_CLIMBING = 'Press any button to start climbing'
 
 # Constants
-CM_PER_STEP = 80
+CM_PER_STEP = 60.5
 ACCEPTABLE_BEARING_ERROR_STAIONARY = 20 # degrees
 ACCEPTABLE_BEARING_ERROR_MOVING = 15 # degrees
 NUM_STEPS_BEFORE_CORRECTING = 2
