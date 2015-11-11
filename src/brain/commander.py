aeaa83591917fcf9cd2c50725563f246c5122b21
seowyanyi <seow.yanyi@gmail.com>
2015-11-12 02:12:49 +0800

    check pedometer before even starting

--- src/brain/commander.py
+++ src/brain/commander.py
@@ -9,22 +9,51 @@ import mapper.mapper as mapper
 import pedometer
 import time
 import RPi.GPIO as GPIO
+imuQueue = queueManager.IMU_QUEUE
+audioQueue = queueManager.AUDIO_QUEUE
+pedometerQueue = queueManager.PEDOMETER_QUEUE
+
+def pedometer_check():
+    # check whether pedometer functioning correctly
+    audioQueue.put('Take five steps now.')
+    steps_taken = 0
+    start_time = int(time.time())
+
+    while int(time.time()) - start_time < 40 and steps_taken < 5:
+        data = pedometerQueue.get(True)
+        if data['type'] == pedometer.Step.FORWARD:
+            audioQueue.put('{} step'.format(steps_taken))
+            steps_taken += 1
+
+    if steps_taken < 5:
+        audioQueue.put('You took less than five steps within the given time')
+
 
 def start(debug):
-    imuQueue = queueManager.IMU_QUEUE
-    audioQueue = queueManager.AUDIO_QUEUE
-    pedometerQueue = queueManager.PEDOMETER_QUEUE
 
     pedometer_thread = pedometer.PedometerThread(
         threadName='pedometer', imuQueue=imuQueue, pedometerQueue=pedometerQueue)
     pedometer_thread.daemon = True
     pedometer_thread.start()
 
+
+    sensor_thread = serialmod.SensorManagerThread(
+        threadName='sensor Manager', imuQueue=imuQueue, audioQueue=audioQueue)
+    sensor_thread.daemon = True
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.output(7, False)
+    time.sleep(1)
+    GPIO.output(7, True)
+    sensor_thread.start()
+
+
     if not debug:
         audio_thread = audio.AudioDispatcherThread(
             threadName='audio Dispatcher', audioQueue=audioQueue)
         audio_thread.daemon = True
         audio_thread.start()
+
+        pedometer_check()
 
         precomputedData = mapper.init_mapper(audioQueue)
         while not precomputedData:
@@ -40,23 +69,8 @@ def start(debug):
         route_manager_thread.daemon = True
         route_manager_thread.start()
 
-
-    sensor_thread = serialmod.SensorManagerThread(
-        threadName='sensor Manager', imuQueue=imuQueue, audioQueue=audioQueue)
-    sensor_thread.daemon = True
-    GPIO.setup(7, GPIO.OUT)
-    GPIO.output(7, False)
-    time.sleep(1)
-    GPIO.output(7, True)
-    sensor_thread.start()
-
-    # Thread 5
-    # kp_thread = KeyPad.KeypadThread(threadName='keypad', keypressQueue=keypadQueue)
-    # kp_thread.daemon = True
-    # kp_thread.start()
-
     while True:
-        time.sleep(1000)
+        time.sleep(2000)
 
 if __name__ == "__main__":
     start(True)
