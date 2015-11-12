fdef3d2ea74ca57fb8f6cbf59c19d6383eeadfa8
Malavika Menon <malavika94@yahoo.com.sg>
2015-11-12 23:47:50 +0800

    debug

--- src/brain/pedometer.py
+++ src/brain/pedometer.py
@@ -142,6 +142,8 @@ def start_pedometer_processing(dataQueue, pedometerQueue, windowSize, atRestLimi
                     long_rest_counter = 0
                     print 'User currently at rest. Heading: {} deg'.format(medianHeading)
                     pedometerQueue.put({'type': Step.AT_REST, 'actual_bearing': medianHeading})
+            if np.amax(data) == np.amin(data):
+                print 'acc x constant value {}'.format(np.amax(data))
 
         # Check whether a step is taken
         if is_downward_swing(data):
